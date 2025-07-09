bd0953f51e19d9fbb60c7f73c0087f4b7a56d0a7
alymeeoow <jdaguidandelena@gmail.com>
2025-07-10 02:29:11 +0800

    new database

--- database/deped_inventory_db (1).sql
+++ database/deped_inventory_db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2025 at 07:36 PM
+-- Generation Time: Jul 09, 2025 at 08:27 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,14 +40,8 @@ CREATE TABLE `deped_inventory_employee_office` (
 --
 
 INSERT INTO `deped_inventory_employee_office` (`office_id`, `office_name`, `office_location`, `created_at`, `updated_at`) VALUES
-('239628', 'Top', '', '2025-07-02 20:56:24', NULL),
 ('422882', '123', '', '2025-07-02 21:39:44', NULL),
-('426334', 'Mid', '', '2025-07-02 20:56:24', NULL),
-('675829', 'Jungle', '', '2025-07-02 20:56:24', NULL),
-('715433', 'My Office', '', '2025-07-03 01:32:27', NULL),
-('754260', 'Bot', '', '2025-07-02 20:56:24', NULL),
-('838296', 'Sideline', '', '2025-07-02 20:56:24', NULL),
-('970316', 'Office of The President', '', '2025-07-03 01:32:27', NULL);
+('754260', '7', '', '2025-07-02 20:56:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -68,11 +62,8 @@ CREATE TABLE `deped_inventory_employee_position` (
 --
 
 INSERT INTO `deped_inventory_employee_position` (`position_id`, `position_title`, `position_description`, `created_at`, `updated_at`) VALUES
-('221375', 'Pos 5', 'Tank', '2025-07-02 20:56:00', NULL),
-('422801', 'Pos 1', 'Gold Lane', '2025-07-02 20:56:00', NULL),
-('451426', 'Pos 3', 'Exp', '2025-07-02 20:56:00', NULL),
-('489639', 'Pos 4', 'Mid', '2025-07-02 20:56:00', NULL),
-('876124', 'Pos 2', 'Jungle', '2025-07-02 20:56:00', NULL);
+('874984', 'position 1 title', '', '2025-07-04 14:12:13', NULL),
+('899245', '7', '', '2025-07-04 14:12:13', NULL);
 
 -- --------------------------------------------------------
 
@@ -95,7 +86,8 @@ CREATE TABLE `deped_inventory_users` (
 --
 
 INSERT INTO `deped_inventory_users` (`user_id`, `email`, `username`, `password`, `role`, `created_at`, `updated_at`) VALUES
-('428651', 'Z3dKS3NPdnRIdG1lMUJtWTVoQWl4QT09OjqVQ8cg1dju0RXTsHt/mM4I', 'miku', '$2y$10$ESJ4ihbK3bn0sAwnEEHb7OFRcaB2B.5dgAhq6WGkuO/rfd.iEZ.xK', 'Employee', '2025-07-02 16:20:33', NULL),
+('428651', 'Z3dKS3NPdnRIdG1lMUJtWTVoQWl4QT09OjqVQ8cg1dju0RXTsHt/mM4I', 'miku', '$2y$10$ESJ4ihbK3bn0sAwnEEHb7OFRcaB2B.5dgAhq6WGkuO/rfd.iEZ.xK', 'Admin', '2025-07-02 16:20:33', NULL),
+('455232', 'Qm8wNHpQWW5jOGozcml0VGZqY3lNWjZ0WXhBalhaY3gxM0E2eE10QXN2TT06OlU1dd6l4E1z8mAY2O5B5ek=', 'milbert', '$2y$10$ID4Es6uM0UoM4m2RuVeXuuNLgeyVQc1IKgKQEzEfV00WO2WG1Kw7C', 'Admin', '2025-07-04 17:45:32', NULL),
 ('979342', 'Z0xPdCtJOWRkS095anRMSG50NFBSclZ1c3dyYm5wZ2xXUFdzOWN3NElsST06OmtzrGXM2SqboerVx/h6kl0=', 'ronson', '$2y$10$LYySZe5oIEt05I8Bd0Xapupnr170ATwdyixb93D4khlBGhBIAO8Fq', 'Deactivate', '2025-07-02 15:15:13', NULL);
 
 -- --------------------------------------------------------
@@ -124,8 +116,9 @@ CREATE TABLE `deped_inventory_user_info` (
 --
 
 INSERT INTO `deped_inventory_user_info` (`info_id`, `user_id`, `first_name`, `middle_name`, `last_name`, `contact_number`, `address`, `profile_photo`, `position_id`, `office_id`, `created_at`, `updated_at`) VALUES
-(234887, '979342', 'Sm9lTE1Dc2FESENwcjhlaEZwd1dLQT09Ojr8HMHHGBI/FMCHpE1r0/kU', 'NEd4SzMrQTNQTkxtQTVUMnBFSGJVZz09Ojoy34Xr+KD293qZGSO794Sd', 'MUIwNDN2R01YS0E5a2xnMUZXSWxEdz09OjpC9jZQlJ7ljQvBGk0IRdhJ', 'd0t4YnBPZ1ZuaHNJcWY1TThzRm96dz09OjraYHB/UB6zXQJ0Dmi8YIO8', 'ZGgyVlAzZ0JYNEdjbjRjM053NmNRdz09OjpQAPO3yCRmx62iAZpzsEk9', '/images/user-profile/ronson_delena.png', '876124', '675829', '2025-07-02 15:15:13', NULL),
-(889896, '428651', 'V2xQR1dkY2poMEV4cElwdWZmeVI3dz09OjppwkpxL/4IQdrB+4zOLONM', 'YnN6SGJYNlUyQWlUeC92Z3RVd1RCUT09OjrOMKtdxyn6jx/DMMH6E+D+', 'QzBuMHBQUDdIbDF4YkJZNzhlNjludz09OjqWhcaXfALaufuu0W780YhJ', 'NlNaNjVwOUwyZXdPaU5MUHZoM3p6QT09Ojp9EUYJzXof8v/nQnNfmq1x', 'aHlCSWVtaHlxcmNkL2lHSGtvb0NoQXV4Z1FKcWFVZytmS09BRE0vV2I3OTk5Q0pHWDIrK0x1RFA2Y0Jja1BIazo6p9JjUNWbFqRh4L+moK29ww==', '/images/user-profile/miku_delena_2.jpg', '876124', '754260', '2025-07-02 16:20:33', NULL);
+(234887, '979342', 'Sm9lTE1Dc2FESENwcjhlaEZwd1dLQT09Ojr8HMHHGBI/FMCHpE1r0/kU', 'NEd4SzMrQTNQTkxtQTVUMnBFSGJVZz09Ojoy34Xr+KD293qZGSO794Sd', 'MUIwNDN2R01YS0E5a2xnMUZXSWxEdz09OjpC9jZQlJ7ljQvBGk0IRdhJ', 'd0t4YnBPZ1ZuaHNJcWY1TThzRm96dz09OjraYHB/UB6zXQJ0Dmi8YIO8', 'ZGgyVlAzZ0JYNEdjbjRjM053NmNRdz09OjpQAPO3yCRmx62iAZpzsEk9', '/images/user-profile/ronson_delena.png', NULL, NULL, '2025-07-02 15:15:13', NULL),
+(639723, '455232', 'NFNuL1p1TGlRdzB6Q3Q2VzBxelViUT09OjrxHMK/dmIPut8KVruGFVeh', NULL, 'Y2oveFpSamN6ZC8xT0UySE1uNHhzQT09Ojpt3Sl7vrulgEw5JmTtCA6c', 'ZDJvOFdNMFNLSENnRGV5dFpLcFdXUT09OjrmVGnay1IH0dZv8MuJMW2Z', 'cytOUkR4WngxbXdBSDhUOUorYzBIQT09Ojq+cSq2i1NcT/nTkOK+STUl', NULL, '874984', '422882', '2025-07-04 17:45:32', NULL),
+(889896, '428651', 'MEdudG8wQmc1dzNHY0RJUHR5R3RMQT09Ojq5xl1QlIeva+f9BixUIK4h', 'QVRVWElRSUY1aW9kNU5LbGRmMmRqQT09Ojr9cXYD0cRqdzIfl7UsmZ6G', 'MUZRbU9DWVZZR3V1TndXMG1rbjBkQT09Ojp0MfOjOO0RNg3woM8LMQFp', 'WW1qQXB0bjFZaTFZSzZkMjRKWFVndz09OjqxqCjDg5G8uFtKCTqe/CEP', 'Wm9uSFV6VjlCbW9RWTA4WU1FQmFnajdBVFIwdUkydFZycEFrRUs3bS9qRlBFWmdUQjFzdjVRTi9zU0hOdkJvWTo64Q/ob5/13+WeGofWz/r1zA==', '/images/user-profile/miku_delena_2.jpg', '899245', '754260', '2025-07-02 16:20:33', NULL);
 
 --
 -- Indexes for dumped tables
--- templates/dashboard/html/addPosition.php
+++ templates/dashboard/html/addPosition.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../../../config/authProtect.php';
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Add Position</title>
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <link rel="stylesheet" href="/styles/dashboard.css">
   <link rel="stylesheet" href="/styles/addOffPos.css">
